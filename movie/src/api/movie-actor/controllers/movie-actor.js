f2a63e93cb3716f0760387a0284b0e3d335d5b36
kgopotso <kgopotso.maloma@dadevs.co.za>
2023-01-05 15:10:12 +0200

    movie

---
+++ movie/src/api/movie-actor/controllers/movie-actor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * movie-actor controller
+ */
+
+const { createCoreController } = require('@strapi/strapi').factories;
+
+module.exports = createCoreController('api::movie-actor.movie-actor');
---
+++ movie/src/api/movie-actor/routes/movie-actor.js
@@ -0,0 +1,9 @@
+'use strict';
+
+/**
+ * movie-actor router
+ */
+
+const { createCoreRouter } = require('@strapi/strapi').factories;
+
+module.exports = createCoreRouter('api::movie-actor.movie-actor');
